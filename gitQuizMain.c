4c48c29d27dda6fc6b1207741a640e43eb8c1f39
sachdevsUoG <116667759+sachdevsUoG@users.noreply.github.com>
2022-11-01 13:09:12 -0400

    Update gitQuizMain.c

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -7,9 +7,10 @@ int main(void)
     printf("Enter 'y' and press enter if you know how to use Git!\n");
     scanf(" %c", &val);
 
-    if(val == 'y'){
+    if(val == 'y')
         printf("You know how to use Git!");
-    }
+    else
+        printf("you don't know how to use git");
 
     printf("\n");
 
